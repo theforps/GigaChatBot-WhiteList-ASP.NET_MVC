79c1fc0744078885478b5549896b32e35f47c35e
well - wisher <104650912+theforps@users.noreply.github.com>
2023-12-07 20:45:31 +0300

    Update Consts.cs

--- GigaChat_Bot/resourses/Consts.cs
+++ GigaChat_Bot/resourses/Consts.cs
@@ -4,10 +4,10 @@ namespace GigaChat_Bot.resourses;
 
 public static class Consts
 {
-    public const string BotApi = "6310507674:AAGrf9d3lFQmfUS3KjwvhUkc9HfLQ0wOxjE";
+    public const string BotApi = "";
 
-    public const string GptToken = "Njk3ZDBiY2EtNmU1Zi00NmFlLThjNTQtMjVkY2I3NmNmYjhlOjY2ZGRiODlkLTQ2N2UtNGNmNS05YjA5LWYyNmYzOWU3ZjZlYg==";
-    public const string GptUid = "6f0b1291-c7f3-43c6-bb2e-9f3efb2dc98e";
+    public const string GptToken = "";
+    public const string GptUid = "";
 
     public const string GptUrl = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions";
     public const string GptUrlReg = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth";
